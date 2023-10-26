21aa32a26ee78b87be07c8ed335cb728ccb650d6
saradarideveloper <MY_NAME@example.com>
2023-10-26 08:18:18 +0330

    add: initial files2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/GPT3",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/GPT3/'
+  }
+
+  return config
 })
