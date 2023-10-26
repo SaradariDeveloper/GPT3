625080a8d4f6a8e410559bed8e2a391a027d3b7b
saradarideveloper <MY_NAME@example.com>
2023-10-26 07:50:14 +0330

    add: initial files

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Navbar from "./navbar/Navbar";
+
+import Brand from "./brand/Brand";
+
+import CTA from "./cta/CTA";
+export {
+    Navbar,
+    CTA,
+    Brand,
+
+}
---
+++ src/containers/index.js
@@ -0,0 +1,15 @@
+import WhatGPT3 from "./whatGPT3/WhatGPT3";
+import Header from "./header/Header";
+import Features from "./features/Features";
+import Possibility from "./possibility/Possibility";
+import Blog from "./blog/Blog";
+import Footer from "./footer/Footer";
+export {
+    WhatGPT3,
+    Header,
+    Features,
+    Possibility,
+    Blog,
+    Footer,
+
+}
